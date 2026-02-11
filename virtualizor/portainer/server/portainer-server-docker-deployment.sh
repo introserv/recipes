368140e683fe411280693e0986a1711ca1958e1b
0sorkon <osorkon@ukr.net>
2026-02-11 14:54:14 +0200

    Update portainer-server-docker-deployment.sh
    added iptables rule for lo int

--- virtualizor/portainer/server/portainer-server-docker-deployment.sh
+++ virtualizor/portainer/server/portainer-server-docker-deployment.sh
@@ -24,6 +24,7 @@ extif=$(ip r | grep default | cut -d " " -f 5)
 iptables -I INPUT -i $extif -p tcp -m tcp --dport 22 -j ACCEPT
 iptables -I INPUT -i $extif -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 iptables -I DOCKER-USER -i $extif -p tcp -m tcp -m multiport --dports 80,443,8000,9001 -j ACCEPT
+iptables -I INPUT -i lo -j ACCEPT
 iptables -P INPUT DROP
 netfilter-persistent save
 docker compose up -d
