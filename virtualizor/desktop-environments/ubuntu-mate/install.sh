97e949d1628ccbe35685a0c15bc603d7690aa10c
0sorkon <osorkon@ukr.net>
2026-02-18 12:37:46 +0200

    Create install.sh
    first commit

---
+++ virtualizor/desktop-environments/ubuntu-mate/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Install Mate DE
+# v18-feb-26
+#
+
+apt update && apt -qq -y full-upgrade
+apt install ubuntu-mate-desktop
+[[ $? == "0" ]] && echo "[Seat:*]\nallow-guest=false\n" > /etc/lightdm/lightdm.conf.d/99-no-guest-session.conf || echo "Failed to install DE" >> /root/recipe.log
+exit 0
