4ee87564d9f1149e0f04d98358cdba02988bae75
0sorkon <osorkon@ukr.net>
2026-02-18 16:52:39 +0200

    Update install.sh

--- virtualizor/desktop-environments/ubuntu-mate/install.sh
+++ virtualizor/desktop-environments/ubuntu-mate/install.sh
@@ -8,4 +8,4 @@ DEBIAN_FRONTEND=noninteractive
 apt update && apt -qq -y full-upgrade
 apt -qq -y install ubuntu-mate-desktop
 [[ $? == "0" ]] && echo "[Seat:*]\nallow-guest=false\n" > /etc/lightdm/lightdm.conf.d/99-no-guest-session.conf || echo "Failed to install DE" >> /root/recipe.log
-reboot
+# reboot
