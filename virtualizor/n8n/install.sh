f6631ca8361a92a3c72815c82dd9309d0944b794
0sorkon <osorkon@ukr.net>
2026-02-25 18:28:41 +0200

    Update install.sh

--- virtualizor/n8n/install.sh
+++ virtualizor/n8n/install.sh
@@ -26,7 +26,7 @@ rm $targetDir/docker-compose.yaml.tmp
 openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout $targetDir/ssl/default.key -out $targetDir/ssl/default.crt -subj "/C=EU/O=INTROSERV/OU=CustomerService/CN=app.introserv.cloud"
 extif=$(ip r | grep default | cut -d " " -f 5)
 docker compose up -d
-sleep 10
+sleep 30
 # wait for cont to become heathly for 60sec to apply user config
 i='"unhealthy"';a=0
 while [[ $a -le 60 ]]; do
@@ -44,7 +44,7 @@ while [[ $a -le 60 ]]; do
 done
 iptables -I INPUT -i $extif -p tcp -m tcp --dport 22 -j ACCEPT
 iptables -I INPUT -i $extif -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
-iptables -I DOCKER-USER -i $extif -p tcp -m tcp -m multiport --dports 80,443,8000,9001 -j ACCEPT
+iptables -I DOCKER-USER -i $extif -p tcp -m tcp -m multiport --dports 80,443,5678 -j ACCEPT
 iptables -I INPUT -i lo -j ACCEPT
 iptables -P INPUT DROP
 #netfilter-persistent save
